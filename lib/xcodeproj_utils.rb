55a4edc01c002b686777d740a6e9317de3b64e65
Matsumoto Taichi <taichino@gmail.com>
2014-02-09 18:33:59 -0500

    add --header_only, --source_only options

--- lib/xcodeproj_utils.rb
+++ lib/xcodeproj_utils.rb
@@ -2,7 +2,7 @@ require "xcodeproj_utils/version"
 require "Xcodeproj"
 
 module XcodeprojUtils
-  def self.wc(proj_name, target_name)
+  def self.wc(proj_name, target_name, header_only=false, source_only=false)
     proj = Xcodeproj::Project::open(proj_name)
 
     for t in proj.targets
@@ -15,26 +15,35 @@ module XcodeprojUtils
       abort("#{target_name} is not found in #{proj_name}")
     end
 
-    sources = []
-    for file in target.source_build_phase.files_references
-      if file.last_known_file_type and file.last_known_file_type.include? "sourcecode"
-        sources.push("'#{file.real_path}'")
+    all = !(header_only || source_only) # default is all
+    count_source = lambda do
+      sources = []
+      for file in target.source_build_phase.files_references
+        if file.last_known_file_type and file.last_known_file_type.include? "sourcecode"
+          sources.push("'#{file.real_path}'")
+        end
       end
+      file_params = sources.join(' ')
+      source_total = %x{wc -l #{file_params}}
+      return source_total.lines[-1].split.first.to_i
     end
-    file_params = sources.join(' ')
-    source_total = %x{wc -l #{file_params}}
-    source_total = source_total.lines[-1].split.first.to_i
 
-    headers = []
-    for file in proj.files
-      if file.path.end_with? ".h"
-        headers.push("'#{file.real_path}'")
+    count_header = lambda do
+      headers = []
+      for file in proj.files
+        if file.path.end_with? ".h"
+          headers.push("'#{file.real_path}'")
+        end
       end
+      file_params = headers.join(' ')
+      header_total = %x{wc -l #{file_params}}
+      return header_total.lines[-1].split.first.to_i
     end
-    file_params = headers.join(' ')
-    header_total = %x{wc -l #{file_params}}
-    header_total = header_total.lines[-1].split.first.to_i
 
+    puts all, source_only, header_only
+    source_total = header_total = 0
+    source_total = count_source.call if all or source_only
+    header_total = count_header.call if all or header_only
     source_total + header_total
   end
 end
