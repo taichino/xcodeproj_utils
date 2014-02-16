219e2434f214557b2a98f0a41e5d2dc4f38a3bb2
Matsumoto Taichi <taichino@gmail.com>
2014-02-16 17:08:09 -0500

    coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'xcodeproj_utils'
 
+require 'coveralls'
+Coveralls.wear!
+
 def fixture_path(path)
   File.join(File.dirname(__FILE__), "fixtures", path)
 end
